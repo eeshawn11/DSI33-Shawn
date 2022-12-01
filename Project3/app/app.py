af672d942bf4dad5d83fd1d0503e74cdf6bb626a
eeshawn <115083646+eeshawn11@users.noreply.github.com>
2022-12-01 23:16:10 +0800

    Update app folder and presentation slides

--- Project3/app/app.py
+++ Project3/app/app.py
@@ -1,21 +1,45 @@
-from flask import Flask,render_template,url_for,request
+from flask import Flask,render_template,request
 import joblib
-from sklearn.pipeline import Pipeline
 from string import punctuation
 from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS
 from nltk import WordNetLemmatizer
 import re
-wn = WordNetLemmatizer()
 
 def clean_text(text):
-    # remove URLs and tags in [] or ()
-    text = re.sub(r'/\[.+\]|\(.+\)|https?:\/\/.+\b', "", text.lower()) 
+    '''
+    Prepare text for modelling
+    
+    Parameters
+    ----------
+    text : str
+        String text to clean, lemmatize and drop stopwords
+    '''
+    wn = WordNetLemmatizer()
+
+    # add extra stopwords
+    extra_stopwords = [
+        "im", "ve", "ive", "hello", "view",
+        "poll", "dont", "know", "ampx200b",
+        "wa", "ha", "just", "like",
+        "got", "did", "bootcamp",
+        "bootcamps", "boot", "camp",
+        "feel", "free", "want", "thanks",
+    ]
+    custom_stopwords = ENGLISH_STOP_WORDS.union(extra_stopwords)
+
+    # convert to lower case, remove URLs and tags in [] or ()
+    text = re.sub(r"/\[.+\]|\(.+\)|https?:\/\/.+\b", "", text.lower())
+    # remove punctuation
     text_nopunct = "".join([char for char in text if char not in punctuation])
-    tokens = re.split(r'\W+', text_nopunct)
-    # lemmatize words before dropping stopwords
+    # tokenize
+    tokens = re.split(r"\W+", text_nopunct)
+    # lemmatize
     tokens_lm = [wn.lemmatize(token) for token in tokens if len(token)]
-    text_cleaned = [token for token in tokens_lm if token not in ENGLISH_STOP_WORDS]
-    return text_cleaned if len(text_cleaned) else None
+    # drop stopwords
+    text_cleaned = " ".join(
+        [token for token in tokens_lm if token not in custom_stopwords]
+    )
+    return text_cleaned if len(text_cleaned) else ""
 
 app = Flask(__name__)
 
@@ -25,33 +49,23 @@ def home():
 
 @app.route('/', methods=['POST'])
 def predict():
-	# df= pd.read_csv("records_adhd_anxiety.csv")
-	# # Features and Labels
-	# df['label'] = df['class'].map({'ham': 0, 'spam': 1})
-	# X = df['message']
-	# y = df['label']
-	
-	# # Extract Feature With CountVectorizer
-	# cv = CountVectorizer()
-	# X = cv.fit_transform(X) # Fit the Data
-	# from sklearn.model_selection import train_test_split
-	# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
-	# #Naive Bayes Classifier
-
-	# clf = MultinomialNB()
-	# clf.fit(X_train,y_train)
-	# clf.score(X_test,y_test)
-	#Alternative Usage of Saved Model
-	# joblib.dump(clf, 'NB_spam_model.pkl')
-	# NB_spam_model = open('NB_spam_model.pkl','rb')
+	# load model
 	pipe = joblib.load('subreddit_model.pkl')
 
 	if request.method == 'POST':
 		message = request.form['message']
-		data = clean_text(message)
-		# include the clean text function here?
-		my_prediction = pipe.predict(data)
-	return render_template("home.html", prediction=my_prediction)
+		if not message:
+			error = 'Please input your message.'
+			return render_template("home.html", error=error)
+		elif message:
+			data = clean_text(message)
+			if data == "":
+				error = 'Sorry, no keywords found. Please try again with a different message.'
+				return render_template("home.html", error=error)
+			elif data:
+				my_prediction = pipe.predict([data])
+				return render_template("home.html", prediction=my_prediction, message=data)
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
